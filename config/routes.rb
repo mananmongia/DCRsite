c9cb0663b099ea0e0f5977b7805c8c1824731d90
Manan Mongia <mongiam4@gmail.com>
2018-08-02 02:01:43 +0530

    intigrated friendly routes for blogs posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact' , to: "pages#contact"
   resources :blogs
-  root to: "pages#home"
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
