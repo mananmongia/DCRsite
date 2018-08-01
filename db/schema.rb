938e5aed5c91b6e27f3b909b35ffb4b7bc084e7c
Manan Mongia <mongiam4@gmail.com>
2018-08-01 13:25:53 +0530

    created seed data or sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_31_204026) do
+ActiveRecord::Schema.define(version: 2018_08_01_033451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_07_31_204026) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.text "discription"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
