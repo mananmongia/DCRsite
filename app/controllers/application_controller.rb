ad7a52b50606cdf27766308c7ba06e52abdfb319
Manan Mongia <mongiam4@gmail.com>
2018-08-13 05:19:02 +0530

    completed authentication with virtual attrs for user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
 
-
-
-
   before_action :configure_permitted_paramerters,if: :devise_controller?
+
   def configure_permitted_paramerters
     devise_parameter_sanitizer.permit(:sign_up , keys:[:name])
     devise_parameter_sanitizer.permit(:account_update , keys:[:name])
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,12 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
   has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
   validates_presence_of :title,:body,:main_image,:thumg_image
   after_initialize :set_defaults
   def set_defaults
-    self.main_image ||= Placeholder.image_generator('600','400')
-    self.thumg_image ||= Placeholder.image_generator('150','200')
+    self.main_image ||= DeviseWhitelist.image_generator('600', '400')
+    self.thumg_image ||= DeviseWhitelist.image_generator('150', '200')
 
 
 
