004eca9b8bf9d5cd91dce9a6460dc3d29db09118
Manan Mongia <mongiam4@gmail.com>
2018-08-19 12:33:47 +0530

    pry binding / byebug

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
-
+  before_action :set_portfolio_item, only: [:edit,:show,:update,:destroy]
   def index
     @portfolio_items = Portfolio.all
   end
@@ -11,7 +11,6 @@ class PortfoliosController < ApplicationController
 
   def create
     @portfolio_items = Portfolio.new(portfolio_params)
-
     respond_to do |format|
       if @portfolio_items.save
         format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
@@ -24,12 +23,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_items = Portfolio.find(params[:id])
-
   end
 
   def update
-    @portfolio_items = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_items.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
@@ -42,11 +38,9 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'record was removed.' }
@@ -61,5 +55,10 @@ class PortfoliosController < ApplicationController
                                       :body,
                                       technologies_attributes: [:name]
                                       )
+  end
+
+  private
+  def set_portfolio_item
+    @portfolio_items = Portfolio.find(params[:id])
   end
 end
